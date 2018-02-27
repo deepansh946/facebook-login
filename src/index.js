b985c7d4ae62db10c8d81b3c925aa2a2922d70fa
Deepansh Bhargava <deepansh940@gmail.com>
2018-02-27 15:24:19 +0530

    Facebook login backend done

--- src/index.js
+++ src/index.js
@@ -1,36 +1,56 @@
 import Hapi from "hapi";
 import mongoose from "mongoose";
+import Bell from "bell";
 
-const server = Hapi.Server({
-  port: 3000,
-  host: "localhost"
-});
+const internals = {};
 
-// mongoose.connect(config.database, {
-//   useMongoClient: true,
-//   connectTimeoutMS: 1000
-// });
+internals.start = async function() {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost"
+  });
 
-// const db = mongoose.connection;
-// db
-//   .on("error", console.error.bind(console, "connection error:"))
-//   .once("open", () => {
-//     console.log("Mongo is live!");
-//   });
+  await server.register(Bell);
 
-server.route({
-  method: "GET",
-  path: "/",
-  handler: () => "Hello there"
-});
+  server.auth.strategy("facebook", "bell", {
+    provider: "facebook",
+    password:
+      "!@#!#!@^#%&!@%#&!@%#^!@#!#!@^#%&!@%#&!@%#^!@#!#!@^#%&!@%#&!@%#^!@#!#!@^#%&!@%#&!@%#^!@#!#!@^#%&!@%#&!@%#^!@#!#!@^#%&!@%#&!@%#^!@#!#!@^#%&!@%#&!@%#^!@#!#!@^#%&!@%#&!@%#^",
+    isSecure: false,
+    clientId: "146530389495656",
+    clientSecret: "c80448e9168a01260102fe10cc4056c0",
+    location: server.info.uri
+  });
 
-async function start() {
-  try {
-    await server.start();
-    console.log("Server is running bro!");
-  } catch (error) {
-    console.error(error);
-  }
-}
+  server.route({
+    method: "*",
+    path: "/facebook",
+    options: {
+      auth: {
+        strategy: "facebook",
+        mode: "try"
+      },
+      handler: function(request, h) {
+        if (!request.auth.isAuthenticated) {
+          return "Authentication failed due to " + request.auth.error.message;
+        }
 
-start();
+        return (
+          "<pre>" + JSON.stringify(request.auth.credentials, null, 4) + "</pre>"
+        );
+      }
+    }
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: () => "Hello there Deepansh"
+  });
+
+  await server.start();
+
+  console.log("Server started");
+};
+
+internals.start();
